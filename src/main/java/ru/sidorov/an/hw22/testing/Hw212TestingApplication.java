4eaa7cf690885cf0406ade4d74152244e5a9575f
anatolij <anatolij001@gmail.com>
2024-04-18 15:56:19 +0300

    Initial Commit.

---
+++ src/main/java/ru/sidorov/an/hw22/testing/Hw212TestingApplication.java
@@ -0,0 +1,13 @@
+package ru.sidorov.an.hw22.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw212TestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw212TestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sidorov/an/hw22/testing/Hw212TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sidorov.an.hw22.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw212TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
