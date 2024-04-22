ca2bf520aac7fd95a83f00717cee2f30e0f18dd3
anatolij001 <anatolij001@gmail.com>
2024-04-22 17:14:01 +0300

    Homework done. Check it please.

---
+++ src/main/java/ru/sidorov/an/hw22/testing/service/CalculatorService.java
@@ -0,0 +1,11 @@
+package ru.sidorov.an.hw22.testing.service;
+
+public interface CalculatorService {
+    int plus(int num1, int num2);
+
+    int minus(int num1, int num2);
+
+    int multiply(int num1, int num2);
+
+    float divide(int num1, int num2);
+}
